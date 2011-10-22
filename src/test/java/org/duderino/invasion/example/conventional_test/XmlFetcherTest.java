160603487a09d170683945ca6c2f5e8c65b45f60
Joshua Blatt <jtblatt@gmail.com>
2011-10-22 16:51:02 -0700

    more examples

--- src/test/java/org/duderino/invasion/example/conventional_test/XmlFetcherTest.java
+++ src/test/java/org/duderino/invasion/example/conventional_test/XmlFetcherTest.java
@@ -18,10 +18,9 @@ import java.util.concurrent.Future;
 
 @Test
 public class XmlFetcherTest {
-    private static final String URL = "http://musicbrainz.org/ws/1/artist?limit=1&query=madonna";
-    private static final String XML = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><metadata xmlns=\"http://musicbrainz.org/ns/mmd-1.0#\" xmlns:ext=\"http://musicbrainz.org/ns/ext-1.0#\"><artist-list offset=\"0\" count=\"13\"><artist type=\"Person\" id=\"79239441-bfd5-4981-a70c-55c3f15c1287\" ext:score=\"100\"><name>Madonna</name><sort-name>Madonna</sort-name><life-span begin=\"1958-08-16\"/></artist></artist-list></metadata>";
-
     public void functionalTest() throws Exception {
+        final String URL = "http://musicbrainz.org/ws/1/artist?limit=1&query=madonna";
+
         XmlFetcher client = new XmlFetcher();
 
         client.fetch(new String[]{URL}, new XmlFetcher.Callback() {
@@ -45,8 +44,12 @@ public class XmlFetcherTest {
         });
     }
 
-    public void testValidResponseAsync() throws Exception {
+    public void testValidResponseSync() throws Exception {
+        final String URL = "http://musicbrainz.org/ws/1/artist?limit=1&query=madonna";
+
         DefaultHttpAsyncClient mock = new DefaultHttpAsyncClient() {
+            private static final String XML = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><metadata xmlns=\"http://musicbrainz.org/ns/mmd-1.0#\" xmlns:ext=\"http://musicbrainz.org/ns/ext-1.0#\"><artist-list offset=\"0\" count=\"13\"><artist type=\"Person\" id=\"79239441-bfd5-4981-a70c-55c3f15c1287\" ext:score=\"100\"><name>Madonna</name><sort-name>Madonna</sort-name><life-span begin=\"1958-08-16\"/></artist></artist-list></metadata>";
+
             @Override
             public Future<HttpResponse> execute(HttpUriRequest request, FutureCallback<HttpResponse> callback) {
                 try {
@@ -91,8 +94,12 @@ public class XmlFetcherTest {
         assert "Madonna".equals(node.getTextContent());
     }
 
-    public void testValidResponseSync() throws Exception {
+    public void testValidResponseAsync() throws Exception {
+        final String URL = "http://musicbrainz.org/ws/1/artist?limit=1&query=madonna";
+
         DefaultHttpAsyncClient mock = new DefaultHttpAsyncClient() {
+            private static final String XML = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><metadata xmlns=\"http://musicbrainz.org/ns/mmd-1.0#\" xmlns:ext=\"http://musicbrainz.org/ns/ext-1.0#\"><artist-list offset=\"0\" count=\"13\"><artist type=\"Person\" id=\"79239441-bfd5-4981-a70c-55c3f15c1287\" ext:score=\"100\"><name>Madonna</name><sort-name>Madonna</sort-name><life-span begin=\"1958-08-16\"/></artist></artist-list></metadata>";
+
             @Override
             public Future<HttpResponse> execute(HttpUriRequest request, FutureCallback<HttpResponse> callback) {
                 try {
